01750895bb8d6b571404f0e7767636645c3c9cca
beeemege <beeemege@gmail.com>
2017-10-24 14:08:44 -0500

    día 2 parte 1

--- 03authentication/application.js
+++ 03authentication/application.js
@@ -2,4 +2,63 @@ var app = angular.module('MyApp', ['firebase']);
 
 app.controller('MyController', function($scope, $window, $firebaseArray, $firebaseAuth, $firebaseObject, $firebaseStorage){
 	
+	var auth = $firebaseAuth();
+	$scope.authInfo = '';
+
+	$scope.email = ''; $scope.password = '';
+
+	$scope.getInfo = function(){
+		$scope.authInfo = auth.$getAuth();
+	}
+
+	$scope.cerrar = function(){
+		auth.$signOut();
+		$scope.authInfo = 'signOut';
+	}
+
+	$scope.anonimo = function(){
+		auth.$signInAnonymously().then(function(firebaseUser){
+			console.log('signInAnonymously');
+		}).catch(function(error){
+			console.log(error);
+		});
+	}
+
+	$scope.proveedor = function(provider){
+		auth.$signInWithPopup(provider).then(function(firebaseUser){
+			$scope.authInfo = firebaseUser;
+		}).catch(function(error){
+			console.log(error);
+		});
+	}
+
+	$scope.correo = function(){
+		auth.$signInWithEmailAndPassword($scope.email, $scope.password)
+		.then(function(firebaseUser){
+			$scope.authInfo = firebaseUser;
+		}).catch(function(error){
+			console.log(error);
+		});
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
